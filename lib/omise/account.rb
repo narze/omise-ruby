dec8f80bbd1ef3ba7bf1f8c2dbef37fc5af85a79
Robin Clart <robin@omise.co>
2015-11-11 14:24:38 +0700

    Add documentation

--- lib/omise/account.rb
+++ lib/omise/account.rb
@@ -1,6 +1,27 @@
 require "omise/object"
 
 module Omise
+  # `Account` allows you to retrieve an Omise account. Note that the `/account`
+  # endpoint is a singleton resource. But this class isn't a singleton and each
+  # account object that you fetch will have it's own object_id.
+  #
+  # Example:
+  #
+  #     Omise.api_key = OMISE_API_KEY
+  #     account = Omise::Account.retrieve
+  #
+  # Alternatively if you have mutliple accounts you can retrieve all of them
+  # separately by passing the `key:` option:
+  #
+  #     accounts = keys.map do |key|
+  #       begin
+  #         Omise::Account.retrieve(key: key)
+  #       rescue Omise::Error => e
+  #         # deal with exception accordingly
+  #         nil
+  #       end
+  #     end.compact
+  #
   class Account < OmiseObject
     self.endpoint = "/account"
     singleton!
--- lib/omise/charge.rb
+++ lib/omise/charge.rb
@@ -9,10 +9,47 @@ module Omise
   class Charge < OmiseObject
     self.endpoint = "/charges"
 
+    # Retrieves a charge object. Calling this method will issue a single HTTP
+    # request:
+    #
+    #   - GET https://api.omise.co/charges/CHARGE_ID
+    #
+    # Example:
+    #
+    #     charge = Omise::Charge.retrieve(charge_id)
+    #
+    # Returns a new `Omise::Charge` instance if successful and raises an
+    # `Omise::Error` if the request fails.
+    #
     def self.retrieve(id, attributes = {})
       new resource(location(id), attributes).get(attributes)
     end
 
+    # Retrieves a list of charge objects. By default the list will be returned
+    # in reverse chronological order. You can inverse the order to start from
+    # the very first charge in your account by passing `chronological` to the
+    # `order:` option. You can also restrict what charges will be returned by
+    # passing the `from:` and/or `to:` options. Lastly you can paginate the
+    # result by using the `offset:` and `limit:` options.
+    #
+    # You can find out more about pagination and list options by visiting this
+    # page: https://www.omise.co/api-pagination
+    #
+    # Calling this method will issue a single HTTP request:
+    #
+    #   - GET https://api.omise.co/charges
+    #
+    # Examples:
+    #
+    #     # Latest charges
+    #     charges = Omise::Charge.list
+    #
+    #     # First charges
+    #     charges = Omise::Charge.list(order: "chronological")
+    #
+    # Returns a new `Omise::Charge` instance if successful and raises an
+    # `Omise::Error` if the request fails.
+    #
     def self.list(attributes = {})
       List.new resource(location, attributes).get(attributes)
     end
